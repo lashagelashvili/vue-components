6d6e08d63de2aaa3dd0ff386f2f777729e7f6cb8
lashagelashvili <las.gelashvili@gmail.com>
2023-10-05 19:13:13 +0400

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <template>
   <TheHeader title="Remember Me" />
-  <StoredResources :resources="storedResources" />
 </template>
 
 <script>
-import StoredResources from './components/learning-resources/StoredResources.vue';
 import TheHeader from './components/layouts/TheHeader.vue';
 
 export default {
   components: {
-    StoredResources,
     TheHeader,
   },
   data() {
